897b7cbefa6b28d6f0774ad1dcbbb0fbfb1f01a1
Richard Jackson <rdjack21@gmail.com>
2015-10-17 22:21:34 -0500

    Add logging dependencies to pom and basic test of asciidoctor in main

--- src/main/java/net/rslt/asciidoctor/App.java
+++ src/main/java/net/rslt/asciidoctor/App.java
@@ -1,13 +1,20 @@
 package net.rslt.asciidoctor;
 
+import java.util.HashMap;
+import org.asciidoctor.Asciidoctor;
+
 /**
- * Hello world!
+ * This application is used to parse asciidoctor files with out the need to 
+ * install ruby. 
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+        Asciidoctor adoc = Asciidoctor.Factory.create();
+		String html = adoc.convert("Writing AsciiDoc is _easy_!", 
+				new HashMap<String, Object>());
+		System.out.println(html);
+	}
 }
