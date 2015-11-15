c8c78e014a01455d2f3764e2a5ecf5f51b28fae7
Richard Jackson <rdjack21@gmail.com>
2015-11-15 17:04:35 -0600

    Added build packaging, Just leaving main simple for now.

--- src/main/java/net/rslt/asciidoctor/App.java
+++ src/main/java/net/rslt/asciidoctor/App.java
@@ -1,20 +1,29 @@
 package net.rslt.asciidoctor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.HashMap;
 import org.asciidoctor.Asciidoctor;
 
 /**
- * This application is used to parse asciidoctor files with out the need to 
- * install ruby. 
+ * This application is used to parse asciidoctor files with out the need to install ruby.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Asciidoctor adoc = Asciidoctor.Factory.create();
-		String html = adoc.convert("Writing AsciiDoc is _easy_!", 
-				new HashMap<String, Object>());
-		System.out.println(html);
-	}
+public class App {
+
+  static String fileToProcess = "/var/code/server/rslt-root/docs/index.adoc";
+  
+  public static void main(String[] args) {
+    Asciidoctor adoc = Asciidoctor.Factory.create();
+
+    int argCount = 0;
+    for(String param: args) {
+      adoc.convertFile(new File(param), new HashMap<String, Object>());
+     System.out.printf("Processed file: %s", param);
+    }
+
+  }
 }
