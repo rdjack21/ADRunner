72c4a0a2a01f9eff385ce7a17f4bded94af85f2c
Richard Jackson <rdjack21@gmail.com>
2016-05-08 23:06:18 -0500

    Clean up imports

--- src/main/java/net/rslt/asciidoctor/App.java
+++ src/main/java/net/rslt/asciidoctor/App.java
@@ -1,10 +1,5 @@
 package net.rslt.asciidoctor;
 
-import java.io.File;
-import java.util.HashMap;
-import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.Attributes;
-import org.asciidoctor.Options;
 import org.asciidoctor.cli.AsciidoctorInvoker;
 
 /**
@@ -15,7 +10,7 @@ public class App {
 
   public static void main(String[] args) {
     
-    // Use the AsciidoctorJ command line processor instead of out own. 
+    // Use the AsciidoctorJ command line processor instead of our own. 
     // This means we can use the same options as the ruby interface that 
     // is documented in the official documentation. 
     
